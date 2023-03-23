0c75a6a9a7924bd2a5dcabd582f068682209f6a5
Sarah Ferrera <123436089+sarahferrera@users.noreply.github.com>
2023-03-23 15:03:51 -0400

    Listo con posibilidad de mejoras visuales

---
+++ script.js
@@ -0,0 +1,73 @@
+const textArea = document.querySelector(".entrada-texto")
+const mensaje = document.querySelector(".mensaje")
+
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function botonEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = ""; 
+    mensaje.style.backgroundImage = "none"
+
+}
+
+function encriptar(stringEncriptada){
+   let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"], ["u", "ufat"]]; 
+   stringEncriptada = stringEncriptada.toLowerCase()
+
+   for(let i = 0; i < matrizCodigo.length; i++){
+        if (stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo [i][1])
+
+
+        } 
+    }
+    return stringEncriptada
+}
+
+function botonDesencriptar(){
+    const textoDesencriptado = desencriptar(textArea.value)
+    mensaje.value = textoDesencriptado
+    textArea.value = ""; 
+    mensaje.style.backgroundImage = "none"
+}
+
+function desencriptar(stringDesencriptada){
+   let matrizCodigo = [["enter","e"],["imes", "i"],["ai", "a"],["ober", "o"], ["ufat", "u"]]; 
+   stringDesencriptada = stringDesencriptada.toLowerCase()
+
+   for(let i = 0; i < matrizCodigo.length; i++){
+        if (stringDesencriptada.includes(matrizCodigo[i][0])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo [i][1])
+
+        } 
+    }
+    return stringDesencriptada
+}
+
+const copyButton = document.querySelector(".botonCopiar");
+
+copyButton.addEventListener("click", () => {
+    const mensajeValue = mensaje.value;
+
+    const copyText = mensajeValue;
+
+    navigator.clipboard.writeText(copyText)
+        .then(() => {
+            console.log('Texto copiado al portapapeles');
+        })
+        .catch((error) => {
+            console.error('Error al copiar el texto: ', error);
+        });
+
+});
+
+// OR 
+// function btnCopiar(){
+//     mensaje.select();
+//     document.execCommand("copy");
