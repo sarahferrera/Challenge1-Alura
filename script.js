d6d8f48692e45c59f4edbd38f736c2bb323e8a59
Sarah Ferrera <123436089+sarahferrera@users.noreply.github.com>
2023-05-01 14:43:39 -0400

    Aplicando Prettier

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const textArea = document.querySelector(".entrada-texto")
-const mensaje = document.querySelector(".mensaje")
-
+const textArea = document.querySelector(".entrada-texto");
+const mensaje = document.querySelector(".mensaje");
 
 // La letra "e" es convertida para "enter"
 // La letra "i" es convertida para "imes"
@@ -8,66 +7,75 @@ const mensaje = document.querySelector(".mensaje")
 // La letra "o" es convertida para "ober"
 // La letra "u" es convertida para "ufat"
 
-function botonEncriptar(){
-    const textoEncriptado = encriptar(textArea.value)
-    mensaje.value = textoEncriptado
-    textArea.value = ""; 
-    mensaje.style.backgroundImage = "none"
-
+function botonEncriptar() {
+  const textoEncriptado = encriptar(textArea.value);
+  mensaje.value = textoEncriptado;
+  textArea.value = "";
+  mensaje.style.backgroundImage = "none";
 }
 
-function encriptar(stringEncriptada){
-   let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"], ["u", "ufat"]]; 
-   stringEncriptada = stringEncriptada.toLowerCase()
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
 
-   for(let i = 0; i < matrizCodigo.length; i++){
-        if (stringEncriptada.includes(matrizCodigo[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo [i][1])
-
-
-        } 
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
     }
-    return stringEncriptada
+  }
+  return stringEncriptada;
 }
 
-function botonDesencriptar(){
-    const textoDesencriptado = desencriptar(textArea.value)
-    mensaje.value = textoDesencriptado
-    textArea.value = ""; 
-    mensaje.style.backgroundImage = "none"
+function botonDesencriptar() {
+  const textoDesencriptado = desencriptar(textArea.value);
+  mensaje.value = textoDesencriptado;
+  textArea.value = "";
+  mensaje.style.backgroundImage = "none";
 }
 
-function desencriptar(stringDesencriptada){
-   let matrizCodigo = [["enter","e"],["imes", "i"],["ai", "a"],["ober", "o"], ["ufat", "u"]]; 
-   stringDesencriptada = stringDesencriptada.toLowerCase()
+function desencriptar(stringDesencriptada) {
+  let matrizCodigo = [
+    ["enter", "e"],
+    ["imes", "i"],
+    ["ai", "a"],
+    ["ober", "o"],
+    ["ufat", "u"],
+  ];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
 
-   for(let i = 0; i < matrizCodigo.length; i++){
-        if (stringDesencriptada.includes(matrizCodigo[i][0])){
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo [i][1])
-
-        } 
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptada.includes(matrizCodigo[i][0])) {
+      stringDesencriptada = stringDesencriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
     }
-    return stringDesencriptada
+  }
+  return stringDesencriptada;
 }
 
 const copyButton = document.querySelector(".botonCopiar");
 
 copyButton.addEventListener("click", () => {
-    const mensajeValue = mensaje.value;
+  const mensajeValue = mensaje.value;
 
-    const copyText = mensajeValue;
+  const copyText = mensajeValue;
 
-    navigator.clipboard.writeText(copyText)
-        .then(() => {
-            console.log('Texto copiado al portapapeles');
-        })
-        .catch((error) => {
-            console.error('Error al copiar el texto: ', error);
-        });
-
+  navigator.clipboard
+    .writeText(copyText)
+    .then(() => {
+      console.log("Texto copiado al portapapeles");
+    })
+    .catch((error) => {
+      console.error("Error al copiar el texto: ", error);
+    });
 });
-
-// OR 
-// function btnCopiar(){
-//     mensaje.select();
-//     document.execCommand("copy");
